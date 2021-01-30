8bfcdc8a312ba31e69f48a74f859e6cd9050c1da
Connor OHara <chuckhead7@gmail.com>
2021-01-29 22:31:38 -0500

    cleaned up

--- src/myeet_types.rs
+++ src/myeet_types.rs
@@ -1,44 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-/*
-#[derive(Serialize, Deserialize, Debug)]
-pub enum MessageType {
-    Identify,
-    NotifyClient,
-    DropClient,
-    NewChatRequest,
-    Auth,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct IncomingMessage {
-    pub message_type: MessageType,
-    pub payload: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct NewChatRequest {
-    pub nickname: String,
-    pub uuid: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum QueueMessageType {
-    Identify,
-    PeerLocation,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct QueueOutgoingMessage {
-    pub message_type: QueueMessageType,
-    pub payload: String,
-}
-*/
-
-/*
- *
- * This enum defines messages that servers send to the Queue
- **/
 #[derive(Serialize, Deserialize, Debug)]
 pub enum QueueMessage {
     /*
